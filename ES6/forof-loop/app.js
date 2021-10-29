7f085507ae9fb8721019a84968f3324afb4b0254
gunmokoo <minju1348@gmail.com>
2021-10-29 23:53:25 +0900

    for, forEach, for of

---
+++ ES6/forof-loop/app.js
@@ -0,0 +1,21 @@
+// for
+
+const abcd = ["a", "b", "c", "d"];
+
+for (let i = 0; i < abcd.length; i++) {
+  console.log(abcd[i]);
+}
+
+// for each
+
+abcd.forEach((item) => console.log(item));
+
+// for of
+
+for (let alpha of abcd) {
+  console.log(alpha);
+}
+
+for (let text of "Hello") {
+  console.log(text);
+}
