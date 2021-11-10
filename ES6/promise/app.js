bfe3bf4959ae7aea900afa8d12914fddbdffc3b7
gunmokoo <minju1348@gmail.com>
2021-11-10 23:55:09 +0900

    use Chaining Promise

--- ES6/promise/app.js
+++ ES6/promise/app.js
@@ -1,13 +1,27 @@
 // promise
 
 const a = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Hello!");
-  }, 3000);
+  setTimeout(resolve, 3000, "Hello!");
 });
 
-console.log(a);
+a.then((result) => console.log(result));
 
-setTimeout(() => {
-  console.log(a);
-}, 3000);
+const b = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000, "Error!");
+});
+
+b.then((result) => console.log(result)).catch((error) => console.log(error));
+
+// chaining promise
+
+const c = new Promise((resolve, reject) => {
+  resolve(2);
+});
+
+const two = (num) => num * num;
+
+c.then(two)
+  .then(two)
+  .then(two)
+  .then((num) => console.log(num))
+  .catch((error) => console.log(error));
