23124e7c9f7386d989b37030b1e88a356d4d1165
gunmokoo <minju1348@gmail.com>
2021-11-12 01:05:33 +0900

    async and await

--- ES6/async/app.js
+++ ES6/async/app.js
@@ -1,1 +1,15 @@
 // async
+
+const getData = async () => {
+  try {
+    const [data, dataa] = await Promise.all([fetch("url"), fetch("url")]);
+    const [dataJson, dataaJson] = await Promise.all(data.json(), dataa.json());
+    console.log(dataJson, dataaJson);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("Finally!");
+  }
+};
+
+getData();
