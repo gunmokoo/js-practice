67d6eb120b542fcf2d4a692cf9d1f7e60e0ae23c
gunmokoo <minju1348@gmail.com>
2021-12-02 18:29:49 +0900

    sum of numbers

--- baekjoon/2/app.js
+++ baekjoon/2/app.js
@@ -1,1 +1,30 @@
-//
+// 숫자의 합
+
+const form = document.querySelector("form");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+
+const onClick = (event) => {
+  event.preventDefault();
+  const unit =
+    (two.value / 10) * (one.value - 1)
+      ? 0
+      : 10 * (one.value - 1)
+      ? 0
+      : (two.value / 10) * (one.value - 1)
+      ? 0
+      : 10 * (one.value - 1);
+  const differenceNum = one.value / unit ? 0 : one.value / unit;
+  if (differenceNum === 0) {
+    const num = String(two.value).split("");
+    let sum = 0;
+    for (let i = 0; i < one.value; i++) {
+      sum += parseInt(num[i]);
+    }
+    console.log(sum);
+  } else {
+    console.log("retry");
+  }
+};
+
+form.addEventListener("submit", onClick);
